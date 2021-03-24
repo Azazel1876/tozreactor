a4bb6efd3da8654fef9f038fbc91c560e281d075
Azazel1876 <51115118+Azazel1876@users.noreply.github.com>
2021-03-24 09:53:43 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <Head />
+    </div>
+  );
+}
+const Head = () => {
+  return (
+    <div className="bk">
+      <img src="hutao.png"/>
+      <div>
+        <header className="panel">
+
+        </header>
+        <div>
+          
+        </div>
+        <footer className="footer">
+
+        </footer>
+      </div>
+      </div>
+  )
+}
+
+export default App;
